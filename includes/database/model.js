78beb045745c1a201a89611cce629e2454c860c4
wwowoww-ops <koji4040404@gmail.com>
2026-08-31 19:22:48 +0100

    Update model.js

--- includes/database/model.js
+++ includes/database/model.js
@@ -4,17 +4,21 @@ module.exports = function (input) {
 	const Users = require("./models/users")(input);
 	const Threads = require("./models/threads")(input);
 	const Currencies = require("./models/currencies")(input);
+	const Pets = require("./models/Pets")(input);
 
 	Users.sync({ force });
 	Threads.sync({ force });
 	Currencies.sync({ force });
+	Pets.sync({ force });
 
 	return {
 		model: {
 			Users,
 			Threads,
 			Currencies,
+			Pets,
 		},
+
 		use: function (modelName) {
 			return this.model[`${modelName}`];
 		}
