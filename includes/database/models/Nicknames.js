0a6ad97bca084e0748b21d2055b3e7eae99c1783
wwowoww-ops <koji4040404@gmail.com>
2026-09-01 20:42:07 +0100

    Create Nicknames.js

---
+++ includes/database/models/Nicknames.js
@@ -0,0 +1,56 @@
+module.exports = function ({ sequelize, Sequelize }) {
+
+  const Nicknames = sequelize.define("Nicknames", {
+
+    // ==================================================
+    // ID
+    // ==================================================
+
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    // ==================================================
+    // المجموعة
+    // ==================================================
+
+    threadID: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: "nickname_member"
+    },
+
+    // ==================================================
+    // العضو
+    // ==================================================
+
+    userID: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: "nickname_member"
+    },
+
+    // ==================================================
+    // الكنية
+    // ==================================================
+
+    nickname: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+
+    // ==================================================
+    // آخر تحديث
+    // ==================================================
+
+    updatedAt: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW
+    }
+
+  });
+
+  return Nicknames;
+};
