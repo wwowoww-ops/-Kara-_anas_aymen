845dfb4cd19afc02c8d2bbe317453e6a3ef6ba2d
wwowoww-ops <koji4040404@gmail.com>
2026-09-01 20:57:15 +0100

    Update model.js

--- includes/database/model.js
+++ includes/database/model.js
@@ -1,29 +1,61 @@
 module.exports = function (input) {
-	const force = false;
 
-	const Users = require("./models/users")(input);
-	const Threads = require("./models/threads")(input);
-	const Currencies = require("./models/currencies")(input);
-	const Pets = require("./models/Pets")(input);
-	const PetCurrency = require("./models/PetCurrency")(input);
+const force = false;
 
-	Users.sync({ force });
-	Threads.sync({ force });
-	Currencies.sync({ force });
-	Pets.sync({ force });
-	PetCurrency.sync({ force });
+const Users =
+	require("./models/users")(input);
 
-	return {
-		model: {
-			Users,
-			Threads,
-			Currencies,
-			Pets,
-			PetCurrency,
-		},
+const Threads =
+	require("./models/threads")(input);
 
-		use: function (modelName) {
-			return this.model[`${modelName}`];
-		}
+const Currencies =
+	require("./models/currencies")(input);
+
+const Pets =
+	require("./models/Pets")(input);
+
+const PetCurrency =
+	require("./models/PetCurrency")(input);
+
+const Nicknames =
+	require("./models/Nicknames")(input);
+
+// ==================================================
+// مزامنة الجداول
+// ==================================================
+
+Users.sync({ force });
+Threads.sync({ force });
+Currencies.sync({ force });
+Pets.sync({ force });
+PetCurrency.sync({ force });
+Nicknames.sync({ force });
+
+// ==================================================
+// Models
+// ==================================================
+
+return {
+
+	model: {
+
+		Users,
+		Threads,
+		Currencies,
+		Pets,
+		PetCurrency,
+		Nicknames
+
+	},
+
+	use: function (modelName) {
+
+		return this.model[
+			`${modelName}`
+		];
+
 	}
-}
+
+};
+
+};
