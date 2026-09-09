f439a3fcf134cb3a7599214f512b8feebaf2f774
wwowoww-ops <koji4040404@gmail.com>
2026-09-09 17:53:23 +0100

    Update handleReaction.js

--- includes/handle/handleReaction.js
+++ includes/handle/handleReaction.js
@@ -1,44 +1,145 @@
 module.exports = function ({ api, models, Users, Threads, Currencies }) {
-    return function ({ event }) {
-        const { handleReaction, commands } = global.client;
-        const { messageID, threadID } = event;
-        if (handleReaction.length !== 0) {
-            const indexOfHandle = handleReaction.findIndex(e => e.messageID == messageID);
+    return async function ({ event }) {
+        try {
+            if (!event) return;
+
+            const { handleReaction, commands } = global.client;
+
+            const messageID = String(event.messageID || "");
+            const threadID = String(event.threadID || "");
+
+            if (!messageID || !threadID) return;
+            if (!Array.isArray(handleReaction) || handleReaction.length === 0) return;
+
+            const indexOfHandle = handleReaction.findIndex(
+                e => String(e.messageID || "") === messageID
+            );
+
             if (indexOfHandle < 0) return;
+
             const indexOfMessage = handleReaction[indexOfHandle];
+
             const handleNeedExec = commands.get(indexOfMessage.name);
 
-            if (!handleNeedExec) return api.sendMessage(global.getText('handleReaction', 'missingValue'), threadID, messageID);
-            try {
-                var getText2;
-                if (handleNeedExec.languages && typeof handleNeedExec.languages == 'object') 
-                	getText2 = (...value) => {
+            if (!handleNeedExec) {
+                return api.sendMessage(
+                    global.getText(
+                        "handleReaction",
+                        "missingValue"
+                    ),
+                    threadID,
+                    messageID
+                );
+            }
+
+            /*
+             * بعض نسخ الـAPI تضع UID صاحب التفاعل
+             * في senderID وبعضها تستخدم userID أو author
+             *
+             * نحافظ على الحدث الأصلي ونضيف reactionUserID
+             * حتى يستطيع الأمر استعماله بدون تخمين.
+             */
+
+            const reactionUserID = String(
+                event.senderID ||
+                event.userID ||
+                event.reactorID ||
+                event.author ||
+                ""
+            );
+
+            const reactionEvent = {
+                ...event,
+
+                /*
+                 * UID الشخص الذي ضغط على التفاعل
+                 */
+                reactionUserID,
+
+                /*
+                 * نحافظ على القيم الأصلية
+                 */
+                messageID,
+                threadID
+            };
+
+            let getText2;
+
+            if (
+                handleNeedExec.languages &&
+                typeof handleNeedExec.languages === "object"
+            ) {
+                getText2 = (...value) => {
                     const react = handleNeedExec.languages || {};
-                    if (!react.hasOwnProperty(global.config.language)) 
-                    	return api.sendMessage(global.getText('handleCommand', 'notFoundLanguage', handleNeedExec.config.name), threadID, messageID);
-                    var lang = handleNeedExec.languages[global.config.language][value[0]] || '';
-                    for (var i = value.length; i > 0x2 * -0xb7d + 0x2111 * 0x1 + -0xa17; i--) {
-                        const expReg = RegExp('%' + i, 'g');
+
+                    if (
+                        !Object.prototype.hasOwnProperty.call(
+                            react,
+                            global.config.language
+                        )
+                    ) {
+                        return api.sendMessage(
+                            global.getText(
+                                "handleCommand",
+                                "notFoundLanguage",
+                                handleNeedExec.config.name
+                            ),
+                            threadID,
+                            messageID
+                        );
+                    }
+
+                    let lang =
+                        handleNeedExec.languages[
+                            global.config.language
+                        ][value[0]] || "";
+
+                    for (let i = value.length; i > 0; i--) {
+                        const expReg = RegExp("%" + i, "g");
                         lang = lang.replace(expReg, value[i]);
                     }
+
                     return lang;
                 };
-                else getText2 = () => {};
-                const Obj = {};
-                Obj.api= api 
-                Obj.event = event 
-                Obj.models = models
-                Obj.Users = Users
-                Obj.Threads = Threads
-                Obj.Currencies = Currencies
-                Obj.handleReaction = indexOfMessage
-                Obj.models= models 
-                Obj.getText = getText2
-                handleNeedExec.handleReaction(Obj);
-                return;
-            } catch (error) {
-                return api.sendMessage(global.getText('handleReaction', 'executeError', error), threadID, messageID);
+            } else {
+                getText2 = () => {};
             }
+
+            const Obj = {
+                api,
+                event: reactionEvent,
+                models,
+                Users,
+                Threads,
+                Currencies,
+                handleReaction: indexOfMessage,
+                getText: getText2
+            };
+
+            await handleNeedExec.handleReaction(Obj);
+
+        } catch (error) {
+            console.error(
+                "[HANDLE REACTION ERROR]",
+                error
+            );
+
+            try {
+                const threadID = String(event?.threadID || "");
+                const messageID = String(event?.messageID || "");
+
+                if (threadID && messageID) {
+                    api.sendMessage(
+                        global.getText(
+                            "handleReaction",
+                            "executeError",
+                            error
+                        ),
+                        threadID,
+                        messageID
+                    );
+                }
+            } catch (_) {}
         }
     };
 };
